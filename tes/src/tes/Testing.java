ada8405b81b1e4779fc43303b5351277f201c00a
sainath_reverieinc <sainath_reverieinc@LalitDash>
2024-03-02 14:23:11 +0530

    `Test

---
+++ tes/src/tes/Testing.java
@@ -0,0 +1,21 @@
+package tes;
+
+import java.util.Scanner;
+
+public class Testing {
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a =sc.nextInt();
+		int b=sc.nextInt();
+		for(int i=a; i<=b; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			
+		}
+		
+	}		
+}
---
+++ tes/src/tes/test1.java
@@ -0,0 +1,8 @@
+package tes;
+
+public class test1 {
+
+	public void main() {
+		System.out.println("Hello");
+	}
+}
